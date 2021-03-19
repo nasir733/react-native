6f3a367013956626da9384e0e0238bd4b4d692af
nasir733 <engnasir733@gmail.com>
2021-03-19 19:49:35 +0500

    made the home page

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useEffect, useState } from "react";
+import Loading from "./loader";
+import * as Loaction from "expo-location";
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const getLoaction = async () => {
+    let { status } = await Location.requestPermissionsAsync();
+    if (status !== "granted") {
+      setErrorMsg("Permission to access location was denied");
+      return;
+    }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    let location = await Location.getCurrentPositionAsync({});
+    setLocation(location);
+  };
+  useEffect(() => {
+    getLocation();
+  });
+  return <Loading location={location} />;
+}
